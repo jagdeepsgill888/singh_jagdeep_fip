5292f4c724dbc1541ac161ccc772df0a0c68d7a6
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2020-08-05 20:36:06 -0400

    added mobile slide menu

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(() => {
+	// "use strict";
+	console.log("fired");
+
+	let burgerCon = document.querySelector("#mainNav");
+
+	function hamburgerMenu() {
+		burgerCon.classList.toggle("slideToggle");
+	}
+
+	button.addEventListener("click", hamburgerMenu);
+})();
