da71a1db6b93fb4ea44a6949e9418b3b77d9cd56
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2020-08-09 06:33:02 -0400

    qill update slider

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@
 
 	let menuToggle = document.querySelector("#menuToggle"),
       burgerCon = document.querySelector("#mainNav");
+			prmoImages = document.querySelector("#prmoImages");
+      houseName = document.querySelector(".house-name");
 
 	function hamburgerMenu() {
     burgerCon.classList.toggle("slideToggle");
@@ -11,5 +13,20 @@
 
 	}
 
+	function animateBanner () {
+
+
+          //Move the banner to the right using the data-offset attribute
+          //we get from the shields
+
+          //600
+          prmoImages.style.right = `${this.dataset.offset * 600}px`;
+
+
+          houseName.textContent =`House ${houseInfo[this.dataset.offset][0]}`;
+
+      }
+
+  sigils.forEach(sigil => sigil.addEventListener("click", animateBanner));
 	menuToggle.addEventListener("click", hamburgerMenu);
 })();
